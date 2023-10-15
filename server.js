ebed800c11bcdb0fe97c5fc68a2f435710ab12e2
rohitpanghal <rohitsinghpanghal10@gmail.com>
2023-10-15 18:05:49 +0530

    limit added

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //System Middlewares
 app.use(cors())
-app.use(express.json());
+app.use(express.json({limit:"200mb"}));
 app.use("/api", router)
 
 app.get("/", (req, res) => {
